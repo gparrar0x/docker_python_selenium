7e6ce9605bd3fb422f76b7d44935014a56c18a91
Gonzalo Parra <gparrar@proton.me>
2023-01-28 21:19:44 -0300

    adding requirements and chromeoptions arguments

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
@@ -13,7 +14,14 @@ def driver(request):
     browser = request.config.getoption("--browser")
     print(f"Creating {browser} driver")
     if browser == "chrome":
-        my_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_prefs = {}
+        chrome_options.experimental_options["prefs"] = chrome_prefs
+        chrome_prefs["profile.default_content_settings"] = {"images": 2}
+        my_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install(), options=chrome_options))
     elif browser == "firefox":
         my_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
     else:
